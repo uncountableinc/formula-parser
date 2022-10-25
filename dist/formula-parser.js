ba6f960260bef5e53cc2cadfd8327ca437aedfbf
Logan Snow <logan@uncountable.com>
2022-10-25 13:58:00 -0400

    fix package build

--- dist/formula-parser.js
+++ dist/formula-parser.js
@@ -1014,6 +1014,7 @@ exports.LOG2E = function() {
 exports.LOG = function(number, base) {
   number = utils.parseNumber(number);
   base = utils.parseNumber(base);
+  console.log('called log with', number, base)
   if (utils.anyIsError(number, base)) {
     return error.value;
   }
@@ -12264,11 +12265,27 @@ var _error = __webpack_require__(2);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _unsupportedIterableToArray(arr) || _nonIterableSpread(); }
+
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _iterableToArray(iter) { if (typeof Symbol !== "undefined" && Symbol.iterator in Object(iter)) return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToArray(arr); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 var SYMBOL = _supportedFormulas["default"];
 exports.SYMBOL = SYMBOL;
 
 function func(symbol) {
   return function __formulaFunction() {
+    for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
+      params[_key] = arguments[_key];
+    }
+
     symbol = symbol.toUpperCase();
     var symbolParts = symbol.split('.');
     var foundFormula = false;
@@ -12277,7 +12294,8 @@ function func(symbol) {
     if (symbolParts.length === 1) {
       if (_formulajs["default"][symbolParts[0]]) {
         foundFormula = true;
-        result = _formulajs["default"][symbolParts[0]].apply(_formulajs["default"], arguments);
+        if (symbol === 'LOG' && params.length === 1) params = [].concat(_toConsumableArray(params), [Math.E]);
+        result = _formulajs["default"][symbolParts[0]].apply(_formulajs["default"], _toConsumableArray(params));
       }
     } else {
       var length = symbolParts.length;
@@ -12296,7 +12314,7 @@ function func(symbol) {
 
       if (nestedFormula) {
         foundFormula = true;
-        result = nestedFormula.apply(void 0, arguments);
+        result = nestedFormula.apply(void 0, _toConsumableArray(params));
       }
     }
 
